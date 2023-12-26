e91f903aba31bbd66b110b69a0b51d08870a2828
Gelfato Gebre <gelf.learing@gmail.com>
2023-12-26 17:15:11 +0300

    making sure that pip3 and python installed first

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
 # install flask from pip3 offcourse using puppet.
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+package { 'python3':
+  ensure => 'installed',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => [Package['python3-pip'], Package['python3']],
 }
