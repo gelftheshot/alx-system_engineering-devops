6952476c1f5e9d7a5aac4506f3b1f95785ef107f
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-01 22:48:41 +0300

    pupet ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,12 @@
 # configering ssh with puppet and also it good practise to do so
-file_line { 'ssh_private_key':
-  ensure  => present,
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^IdentityFile',
-  path    => '/etc/ssh/ssh_config',
-  replace => true,
+file_line { 'Turn off passwd auth':
+  path  => '/home/root/.ssh/config',
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
 }
 
-file_line { 'disable_password_authentication':
-  ensure  => present,
-  line    => 'PasswordAuthentication no',
-  match   => '^PasswordAuthentication',
-  path    => '/etc/ssh/sshd_config',
-  replace => true,
+file_line { 'Declare identity file':
+  path  => '/etc/root/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
 }
