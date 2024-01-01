6b1c6d05dedf540246173b84daa03abe76f13cf0
Gelfato Gebre <gelf.learing@gmail.com>
2024-01-01 22:03:24 +0300

    makeing them in different groups

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,16 @@
 # configering ssh with puppet
-class ssh_config {
-  file_line { 'ssh_private_key':
-    ensure  => present,
-    line    => 'IdentityFile ~/.ssh/school',
-    match   => '^IdentityFile',
-    path    => '/etc/ssh/ssh_config',
-    replace => true,
-  }
+file_line { 'ssh_private_key':
+  ensure  => present,
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^IdentityFile',
+  path    => '/etc/ssh/ssh_config',
+  replace => true,
+}
 
-  file_line { 'disable_password_authentication':
-    ensure  => present,
-    line    => 'PasswordAuthentication no',
-    match   => '^PasswordAuthentication',
-    path    => '/etc/ssh/sshd_config',
-    replace => true,
-  }
+file_line { 'disable_password_authentication':
+  ensure  => present,
+  line    => 'PasswordAuthentication no',
+  match   => '^PasswordAuthentication',
+  path    => '/etc/ssh/sshd_config',
+  replace => true,
 }
