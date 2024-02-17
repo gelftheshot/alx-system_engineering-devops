8fea099b06eb8a412231abb1725cae2b94e69506
Leon Gebre <gelf.learing@gmail.com>
2024-02-17 15:24:36 +0300

    exporting all data to json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Python script to export data in the CSV format.
+"""
+import requests
+import sys
+import json
+if __name__ == "__main__":
+
+    employes = requests.get("https://jsonplaceholder.typicode.com/users")
+    dic = {}
+    for data in employes.json():
+        user_id = data["id"]
+        user_name = data["username"]
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+        user_todos = [x for x in todos.json() if x["userId"] == user_id]
+        name = "todo_all_employees.json"
+        new_todos = [{"username": user_name, "task": dataa["title"], "completed": dataa["completed"]} for dataa in user_todos]
+        dic.update({user_id: new_todos})
+    with open(name, "w") as file:
+        json.dump(dic, file)
