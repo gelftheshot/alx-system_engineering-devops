e448c9190b32113481b4936f982198ee62910050
Leon Gebre <gelf.learing@gmail.com>
2024-02-17 14:00:44 +0300

    exporting json file to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    for a given employee ID, returns information
+    about his/her TODO list progress.
+"""
+import requests
+import sys
+import csv
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+    employes = requests.get("https://jsonplaceholder.typicode.com/users")
+    user_name = [x["username"] for x in employes.json() if x["id"] == user_id]
+    if user_name:
+        user_name = user_name[0]
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    user_todos = [x for x in todos.json() if x["userId"] == user_id]
+    name = str(user_id) + ".csv"
+    with open(name, "w") as file:
+        writer = csv.writer(file)
+        for data in user_todos:
+            row = [data["userId"], user_name, data["completed"], data["title"]]
+            writer.writerow(row)
