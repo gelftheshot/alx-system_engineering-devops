78f34b7b89cc08f99cda4bcdb72735f6167f7e2c
Leon Gebre <gelf.learing@gmail.com>
2024-02-17 15:00:40 +0300

    exporting to json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Python script to export data in the CSV format.
+"""
+import requests
+import sys
+import json
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+    employes = requests.get("https://jsonplaceholder.typicode.com/users")
+    user_name = [x["username"] for x in employes.json() if x["id"] == user_id]
+    if user_name:
+        user_name = user_name[0]
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    user_todos = [x for x in todos.json() if x["userId"] == user_id]
+    name = str(user_id) + ".json"
+    new_todos = [{"task": data["title"], "completed": data["completed"], "username": user_name} for data in user_todos]
+    dic = {user_id: new_todos}
+    with open(name, "w") as file:
+        json.dump(dic, file)
