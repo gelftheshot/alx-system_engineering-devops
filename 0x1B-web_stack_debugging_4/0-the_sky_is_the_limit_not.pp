a51935a4c9844b35f4b15adb206d0f51221157fe
Leon Gebre <gelf.learing@gmail.com>
2024-02-24 16:52:48 +0300

    puppet script to add number of traffic

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Increse trafic limit of the server
+
+exec { 'increse-limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'restart-nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+this is the readme file for 0x1B-web_stack_debugging_4
