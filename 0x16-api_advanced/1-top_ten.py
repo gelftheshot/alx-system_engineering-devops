2f6f378abfc421767137599ef8ed0c4e217d5bde
Leon Gebre <gelf.learing@gmail.com>
2024-02-24 16:32:19 +0300

    fixing unknow bug

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """
 Query the Reddit API and prints the titles of the first 10 hot
-posts listed for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
     """
-    Fetch and print the titles of the first 10 hot posts for a given subreddit.
+    Get the titles of the first 10 hot posts for a given subreddit.
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Reddit Scraper v1.0"}
+    headers = {"User-Agent": "Reddit scrapper 1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    for post in response.json().get("data").get("children"):
+        print(post.get("data").get("title"))
