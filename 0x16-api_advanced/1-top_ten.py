ca093562e1341dedc823d957b033a529f8e4b5ef
Leon Gebre <gelf.learing@gmail.com>
2024-02-24 15:49:36 +0300

    changing the f fromat for string

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,7 +13,7 @@ def top_ten(subreddit):
     Parameters:
     subreddit (str): The name of the subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "My User Agent 1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
